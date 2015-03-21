07e662d116c9121dd4b361d215c717b7719b1b28
yumechi <yumechi@users.noreply.github.com>
2015-03-22 02:56:39 +0900

    ABC 020 D fixed(100point version)

--- RealTime/ABC020/abc020d.py
+++ RealTime/ABC020/abc020d.py
@@ -1,10 +1,7 @@
-# 部分点15点はGET
-
-def lcm(a, b, l):
-    return (a * b) // gcd(a, b, l)
-
+# 100point version(viewing slide)
 def gcd(a, b, l):
     if a % b == 0:
+        l[a] = b
         return b
 
     if l[a % b] > 0:
@@ -21,8 +18,17 @@ def gcd(a, b, l):
     return b
 
 N, K = map(int, input().split())
-sum = 0
+
 modlist = [0 for _ in range(0, K)]
-for i in range(1, N + 1):
-    sum += lcm(i, K, modlist)
+for i in range(0, K):
+    gcd(i, K, modlist)
+
+sum = 0
+for i in range(1, K + 1):
+    M = N - K + i
+    num = N // K if N // K > (M - 1) // K else N // K + 1
+    As = (K + M) * num // 2 if M % K == 0 else (M % K + M) * num // 2
+    gcdsum = (As * K) // modlist[M % K]
+    sum += gcdsum
+
 print(sum % 1000000007)
