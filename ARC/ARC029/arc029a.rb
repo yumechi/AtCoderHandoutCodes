4051cdd5db80ba9f63be1af91c8a7217ff49d8c7
yumechi <yumechi@users.noreply.github.com>
2015-05-20 19:33:35 +0900

    file rename (arc029 -> arc029a)

---
+++ ARC/ARC029/arc029a.rb
@@ -0,0 +1,20 @@
+n = gets.to_i
+li = []
+n.times { li.push gets.to_i }
+maxnum = 999999
+1.upto(2 ** n) { |i|
+	t = i
+	plateA = 0
+	plateB = 0
+	0.upto(n - 1) { |j|
+		if (t & 1) == 1
+			plateA += li[j]
+		else
+			plateB += li[j]
+		end
+		t >>= 1
+	}
+	maxnum = [maxnum, [plateA, plateB].max].min
+}
+
+puts maxnum
