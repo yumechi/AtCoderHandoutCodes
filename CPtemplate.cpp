1d1981c1a8ca7f4dee625ac0aaf06a9d1320f123
yumechi <yumechi@users.noreply.github.com>
2015-09-09 23:13:36 +0900

    C++ template Add

---
+++ CPtemplate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <queue>
+
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+#define FOR(i,a,b) for (int i=(a);i<(b);i++)
+#define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define INF 1<<30
+#define MP make_pair
+#define mp make_pair
+#define pb push_back
+#define PB push_back
+#define DEBUG(x) cout<<#x<<": "<<x<<endl
+#define ll long long
+#define ull unsigned long long
+#define MOD 1000000007
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout.precision(16);
+
+}
