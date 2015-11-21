c135561cf09e44e1686f0c80d629e3cc4c46a393
yumechi <yumechi@users.noreply.github.com>
2015-11-21 16:57:23 +0900

    Delete CPP template because I made template repository

--- RealTime/TTPC2015/ProgGWA.py
+++ RealTime/TTPC2015/ProgGWA.py
@@ -1,11 +1,7 @@
-d = {"t":0, "i":0, "e":0, "c":0, "h":0}
-for c in input():
-    if c not in d:
-        print("No")
-        exit(0)
-    else:
-        d[c] += 1
-if (d["t"] % 2 == 0) and (d["t"] // 2 == d["i"] == d["e"] == d["c"] == d["h"]):
-    print("Yes")
-else:
+from collections import Counter
+
+s = input()
+d = Counter(s)
+# illegal word count
+if len(s) % 6 == 0 or not ((d["t"] % 2 == 0) and (d["t"] // 2 == d["i"] == d["e"] == d["c"] == d["h"])):
     print("No")
