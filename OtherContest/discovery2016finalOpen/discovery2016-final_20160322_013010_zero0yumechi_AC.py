7d40139e8dfd0e074a2b0e463a74667fd5eec7a7
yumechi <yumechi@users.noreply.github.com>
2016-03-22 01:32:27 +0900

    Discovery2016 Final A

---
+++ OtherContest/discovery2016finalOpen/discovery2016-final_20160322_013010_zero0yumechi_AC.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+ 
+n = int(input())
+dic = OrderedDict(zip([int(input()) for _ in range(5)], [i for i in range(1, 6)]))
+prize = {1:100000, 2:50000, 3:30000, 4:20000, 5:10000}
+print("\n".join(map(str, [prize[dic[i]] if i in dic else 0 for i in range(1, n+1)])))
