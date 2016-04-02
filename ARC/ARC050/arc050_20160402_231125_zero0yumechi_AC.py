f21d141398cd2ea723bd0fde1a162eedcb8d2464
yumechi <yumechi@users.noreply.github.com>
2016-04-03 00:18:16 +0900

    ARC050 A, B

---
+++ ARC/ARC050/arc050_20160402_231125_zero0yumechi_AC.py
@@ -0,0 +1,18 @@
+def solve():
+    red, blue = map(int, input().split())
+    needR, needB = map(int, input().split())
+ 
+    low, high = 0, 10 ** 20
+    while high - low > 1:
+        mid = (low + high) // 2
+        if (red >= mid) and  \
+           (blue >= mid) and \
+           (red - mid) // (needR - 1) + (blue - mid) // (needB - 1) >= mid:
+           low = mid
+        else:
+           high = mid
+ 
+    print(low)
+ 
+if __name__=="__main__":
+    solve()
