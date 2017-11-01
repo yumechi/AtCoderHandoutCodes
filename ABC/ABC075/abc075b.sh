077b7caed9d4aead5a53812feb742de22e0bb8e4
yumechi <yumechi0525@gmail.com>
2017-11-02 01:04:51 +0900

    optimization for loop

--- ABC/ABC075/abc075b.sh
+++ ABC/ABC075/abc075b.sh
@@ -7,36 +7,34 @@ for i in `seq ${H}`; do
     arr+=($st)
 done
 
-ans=()
 range=(-1 0 1)
 for i in `seq 0 $((H-1))`; do
-    ansline=''
+    ans=''
+    tl=${arr[$i]}
     for j in `seq 0 $((W-1))`; do
-        tl=${arr[$i]}
         t=${tl:j:1}
-        # echo "${i}, ${j}, ${tl}, ${tl:j:1}"
         if [ ${t} = '#' ]; then 
-            ansline=${ansline}'#'
-        else
-            cnt=0
-            for k in ${range[@]}; do
-                if [ $((i+k)) -lt 0 ] || [ $((i+k)) -ge ${H} ]; then
+            ans=${ans}'#'
+            continue
+        fi
+
+        cnt=0
+        for k in ${range[@]}; do
+            if [ $((i+k)) -lt 0 ] || [ $((i+k)) -ge ${H} ]; then
+                continue
+            fi
+            al=${arr[$((i+k))]}
+            for l in ${range[@]}; do
+                if [ $((j+l)) -lt 0 ] || [ $((j+l)) -ge ${W} ]; then
                     continue
                 fi
-                al=${arr[$((i+k))]}
-                for l in ${range[@]}; do
-                    if [ $((j+l)) -lt 0 ] || [ $((j+l)) -ge ${W} ]; then
-                        continue
-                    fi
-                    a=${al:$((j+l)):1}
-                    # echo "$((i+k)), $((j+l)), ${al}, ${a}, ${cnt}"
-                    if [[ ${a} = '#' ]]; then
-                        let cnt++
-                    fi
-                done
+                a=${al:$((j+l)):1}
+                if [[ ${a} = '#' ]]; then
+                    let cnt++
+                fi
             done
-            ansline=${ansline}${cnt}
-        fi
+        done
+        ans=${ans}${cnt}
     done
-    echo ${ansline} 
+    echo ${ans} 
 done
